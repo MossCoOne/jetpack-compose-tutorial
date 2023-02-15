0e7ff292477230a24dfc1b707151acd0dc2730ba
Kgoputso Moabelo <kgoputso.moabelo@standardbank.co.za>
2023-02-15 11:24:27 +0200

    Add layout basic

--- app/src/main/java/com/example/androidmad/ComposeMainActivity.kt
+++ app/src/main/java/com/example/androidmad/ComposeMainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.androidmad
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidmad.ui.theme.AndroidMadTheme
 
 class ComposeMainActivity : ComponentActivity() {
@@ -17,27 +23,103 @@ class ComposeMainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidMadTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                LandingPageLayout()
+            }
+        }
+    }
+}
+
+/**
+ * Modifiers tell a UI element how to lay out, display, or behave within its parent layout
+ * It's good practice to the consumer ability to customize their views by passing the Modifier as a param
+ *
+ * */
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier, names: List<String>) {
+    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
+        LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+            items(items = names) { name ->
+                Greeting(name = name)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun Greeting(name: String) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidMadTheme {
-        Greeting("Android")
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name")
+            }
+
+            ElevatedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
+
+
+@Preview(showBackground = true, name = "Greeting preview", widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    AndroidMadTheme {
+        //MyApp(names = List(10) { "$it" } )
+        LandingPageLayout()
+    }
+
+}
+
+
+@Composable
+fun BorderedButton(buttonText: String, modifier: Modifier, onClick: () -> Unit) {
+    OutlinedButton(
+        onClick = { onClick }, modifier = modifier
+    ) {
+        Text(text = buttonText)
+    }
+}
+
+@Composable
+fun LandingPageLayout() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        BorderedButton(
+            "Input fields",
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Toast.makeText(context, "Clicked first button", Toast.LENGTH_SHORT).show()
+        }
+        BorderedButton(
+            "Cards",
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Toast.makeText(context, "Clicked Second button", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+
+data class Message(val author: String, val body: String)
--- app/src/main/java/com/example/androidmad/recyclerview/ArtistsActivity.kt
+++ app/src/main/java/com/example/androidmad/recyclerview/ArtistsActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmad.R
 
@@ -121,120 +120,7 @@ class ArtistsActivity : AppCompatActivity() {
             ),
             Artist(
                 "Kabelo", arrayListOf(
-                    Song(title = "1"), private fun getDataSet(): ArrayList<Artist> {
-                        return arrayListOf(
-                            Artist(
-                                "Kabza", arrayListOf(
-                                    Song(
-                                        title = "1",
-                                        albumArtUrl = "https://images.punkapi.com/v2/keg.png"
-                                    ),
-                                    Song(
-                                        title = "2",
-                                        albumArtUrl = "https://images.punkapi.com/v2/2.png"
-                                    ),
-                                    Song(
-                                        title = "3",
-                                        albumArtUrl = "https://images.punkapi.com/v2/keg.png"
-                                    ),
-                                    Song(
-                                        title = "3",
-                                        albumArtUrl = "https://images.punkapi.com/v2/4.png"
-                                    ),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "4"),
-                                    Song(title = "5"),
-                                    Song(title = "6"),
-                                    Song(title = "7"),
-                                    Song(title = "8")
-                                )
-                            ),
-                            Artist(
-                                "Madumane", arrayListOf(
-                                    Song(
-                                        title = "a",
-                                        albumArtUrl = "https://images.punkapi.com/v2/keg.png"
-                                    ),
-                                    Song(
-                                        title = "b",
-                                        albumArtUrl = "https://images.punkapi.com/v2/12.png"
-                                    ),
-                                    Song(
-                                        title = "g",
-                                        albumArtUrl = "https://images.punkapi.com/v2/13.png"
-                                    )
-                                )
-                            ),
-                            Artist(
-                                "Shasha", arrayListOf(
-                                    Song(
-                                        title = "1",
-                                        albumArtUrl = "https://images.punkapi.com/v2/8.png"
-                                    ),
-                                    Song(
-                                        title = "2",
-                                        albumArtUrl = "https://images.punkapi.com/v2/9.png"
-                                    ),
-                                    Song(
-                                        title = "3",
-                                        albumArtUrl = "https://images.punkapi.com/v2/10.png"
-                                    ),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "3"),
-                                    Song(title = "4"),
-                                    Song(title = "e"),
-                                    Song(title = "h")
-                                )
-                            ),
-                            Artist(
-                                "Demthuda ka Mazola", arrayListOf(
-                                    Song(
-                                        title = "a",
-                                        albumArtUrl = "https://images.punkapi.com/v2/keg.png"
-                                    )
-                                )
-                            ),
-                            Artist(
-                                "Zola", arrayListOf(
-                                    Song(title = "1",albumArtUrl = "https://images.punkapi.com/v2/14.png"),
-                                    Song(title = "2",albumArtUrl = "https://images.punkapi.com/v2/15.png"),
-                                    Song(title = "2",albumArtUrl = "https://images.punkapi.com/v2/16.png"),
-                                    Song(title = "2"),
-                                    Song(title = "3"),
-                                    Song(title = "4"),
-                                    Song(title = "e"),
-                                    Song(title = "h")
-                                )
-                            ),
-                            Artist(
-                                "Kabelo", arrayListOf(
-                                    Song(title = "1"),
-                                    Song(title = "2"),
-                                    Song(title = "3"),
-                                    Song(title = "4"),
-                                    Song(title = "e"),
-                                    Song(title = "e"),
-                                    Song(title = "e"),
-                                    Song(title = "e"),
-                                    Song(title = "e"),
-                                    Song(title = "h")
-                                )
-                            )
-                        )
-
-                    }
+                    Song(title = "1"),
                     Song(title = "2"),
                     Song(title = "3"),
                     Song(title = "4"),
@@ -247,6 +133,5 @@ class ArtistsActivity : AppCompatActivity() {
                 )
             )
         )
-
     }
 }
