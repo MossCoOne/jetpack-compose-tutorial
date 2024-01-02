7dce89da385a5ee184ddf6fda9b392ff3c7ba131
Kgoputso Moabelo <kgoputso.moabelo@standardbank.co.za>
2024-01-02 10:27:38 +0200

    - Add basic layout form - Add clickable text in a string

--- app/src/main/java/com/example/androidmad/ComposeMainActivity.kt
+++ app/src/main/java/com/example/androidmad/ComposeMainActivity.kt
@@ -1,4 +1,3 @@
-
 package com.example.androidmad
 
 import android.os.Bundle
@@ -6,21 +5,30 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
+import androidx.compose.material3.TextFieldDefaults.indicatorLine
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,18 +70,18 @@ fun AnnotatedClickableText(text: String, clickableText: String, url: String) {
         }
         pop()
     }
-
+    val mUriHandler = LocalUriHandler.current
     ClickableText(
         text = annotatedText,
         onClick = { offset ->
             annotatedText.getStringAnnotations(
                 tag = "URL", start = offset,
                 end = offset
-            )
-                .firstOrNull()?.let { annotation ->
-                    //Break out to T&C
-                    Log.d("Clicked URL", annotation.item)
-                }
+            ).firstOrNull()?.let { annotation ->
+                //Break out to T&C
+                mUriHandler.openUri(annotation.item)
+                Log.d("Clicked URL", annotation.item)
+            }
         }
     )
 }
@@ -130,9 +138,64 @@ private fun Greeting(name: String) {
 fun GreetingPreview() {
     AndroidMadTheme {
         //MyApp(names = List(10) { "$it" } )
-        CallMeBackForm()
+        //CallMeBackForm()
+        CustomTextField(
+            value ="Saple Value",
+            onValueChange = {it},
+            label = { Text(text = "First name") })
     }
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    label: @Composable() (() -> Unit)? = null,
+    shape: Shape = TextFieldDefaults.filledShape,
+    isError: Boolean = true,
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors(),
+    textStyle: TextStyle = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Normal,
+        color = Color.DarkGray,
+        textAlign = TextAlign.Start
+    )
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val enabled = true
+    val singleLine = true
+
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .background(colorResource(id = R.color.white), shape)
+            .indicatorLine(enabled, isError, interactionSource, colors)
+            .defaultMinSize(
+                minWidth = TextFieldDefaults.MinWidth,
+                minHeight = TextFieldDefaults.MinHeight
+            ),
+        interactionSource = interactionSource,
+        textStyle = textStyle,
+        enabled = enabled,
+        singleLine = singleLine
+    ) {
+        TextFieldDefaults.TextFieldDecorationBox(
+            value = value,
+            visualTransformation = VisualTransformation.None,
+            innerTextField = it,
+            label = label,
+            singleLine = singleLine,
+            enabled = enabled,
+            interactionSource = interactionSource,
+            contentPadding = PaddingValues(0.dp),
+            colors = colors
+        )
+    }
 }
 
 
@@ -169,7 +232,6 @@ fun LandingPageLayout() {
         }
     }
 }
-
 
 
 data class Message(val author: String, val body: String)
--- app/src/main/java/com/example/androidmad/ui/main/ui/CallMeBackForm.kt
+++ app/src/main/java/com/example/androidmad/ui/main/ui/CallMeBackForm.kt
@@ -2,7 +2,6 @@ package com.example.androidmad.ui.main.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.rememberScrollState
@@ -11,14 +10,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import com.example.androidmad.AnnotatedClickableText
+import com.example.androidmad.CustomTextField
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,6 +39,14 @@ fun CallMeBackForm() {
             text = "Please fill your details below so that one of our consultants will contact you and assist you",
             fontFamily = FontFamily.SansSerif,
             modifier = Modifier.padding(8.dp)
+        )
+        var mInput1 by remember{ mutableStateOf("")}
+        CustomTextField(
+            value = mInput1, onValueChange = { mInput1 = it }, modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+                .width(328.dp),
+            label = { Text(text = "First name") }
         )
 
         Spacer(modifier = Modifier.height(2.dp))
@@ -81,7 +88,7 @@ fun CallMeBackForm() {
         )
 
         Text(
-            text = "Notification sent when you transact will be sent to the cellphone number we currently have on record. Please contact us on 0860 123 000 should you want to change the cellphone number",
+            text = "Notification sent when you transact will be sent to the cellphone number we currently have on record. Please contact us on 0860 004 111 should you want to change the cellphone number",
             fontFamily = FontFamily.SansSerif,
             modifier = Modifier.padding(8.dp),
             style = TextStyle(color = Color.Red)
@@ -108,7 +115,7 @@ fun CallMeBackForm() {
             ) {
 
                 val text =
-                    "I agree that the Bank will process the personal information that I have provided to call me back in relation to the product/services that I have expressed interest in. Standard bank keeps all information shared with it confidential in line with it’s "
+                    "I agree that the Bank will process the personal information that I have provided to call me back in relation to the product/services that I have expressed interest in. The bank keeps all information shared with it confidential in line with it’s "
                 val clickableText = "Privacy Policy."
                 val url = "https://developer.android.com/jetpack"
 
